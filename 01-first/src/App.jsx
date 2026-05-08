9908c0889deff4acb2e21de64187fd7112ef8ce6
Mohammad Hasibul Hasan <mohammadhasibulhasanpga4149@gmail.com>
2026-05-08 12:01:10 +0600

    Complete basic setup and understanding file

---
+++ 01-first/src/App.jsx
@@ -0,0 +1,19 @@
+// import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div id="parent">
+      <h1 id="child1">Hello fron child 1</h1>
+      <h1 id="child2">Hello fron child 1</h1>
+    </div>
+
+      <div id="Chacca">
+      <h1 id="child3">Hello fron child 3</h1>
+      <h1 id="child4">Hello fron child 4</h1>
+    </div>
+    </>
+  );
+};
+
+export default App;
