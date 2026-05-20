98ef49805b13923c7196f6ef49e760dfd125fffe
Mohammad Hasibul Hasan <mohammadhasibulhasanpga4149@gmail.com>
2026-05-20 23:53:56 +0600

    complete ui

--- 13-notes-app/src/App.jsx
+++ 13-notes-app/src/App.jsx
@@ -1,13 +1,49 @@
+import { useState } from "react";
+
 const App = () => {
+
+
+ function submitHandler(e) {
+  e.preventDefault();
+  console.log("Submitted....")
+    
+  }
+
+  const [Tittle, setTittle] = useState("");
+
   return (
 
-    <div>
-      <form onSubmit={}>
+    <div className="flex">
 
-        <input type="text" placeholder="Enter your task"></input>
-        <textarea placeholder="Description"></textarea>
+      <div className="h-screen w-1/2">
+        <form onSubmit={(e)=>{submitHandler(e); }} 
+         className=" flex flex-col gap-6  p-10 items-start">
+        <h1 className="text-2xl font-bold py-2">Add Your Notes</h1>
 
-      </form>
+        <input type="text"
+         placeholder="Enter your notes"
+         className="border-2 rounded h-15 px-5 w-1/2">
+         </input>
+
+        <textarea placeholder="Write details"
+        className="border-2 rounded h-35 px-5  w-1/2">
+        </textarea>
+
+        <button className="bg-black text-white px-5 py-2 rounded  w-1/2"> Add Note</button>
+
+         </form>
+      </div>
+
+      <div className="border-2"></div>
+
+  <div className="flex flex-wrap px-5 py-5 p-10 w-1/2 gap-4  h-full overflow-auto">
+     <h1 className="text-2xl font-bold py-2 w-full">Current Notes</h1>
+
+     <div className="h-52 w-40 rounded-2xl bg-amber-400"></div>
+
+     
+
+  </div>
     </div>
      );
 };
